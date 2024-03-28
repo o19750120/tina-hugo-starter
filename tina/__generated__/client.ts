71c126330813cf55223506047b7918f74acd1337
o19750120 <o19750120@gmail.com>
2024-03-28 15:41:02 +0900

    yarn

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: '337030e2accb793281c5a4397c80fabde0a39724', queries,  });
+export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'undefined', queries,  });
 export default client;
   
